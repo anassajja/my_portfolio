058186dcdb3740c9c164b524b43c0f8fe5757286
anassajja <anass.ajja2020@gmail.com>
2024-12-11 12:28:05 +0100

    Commit

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 // File: next.config.js
 
-module.exports = { // eslint-disable-line no-undef
-  webpack: (config, { isServer }) => { // eslint-disable-line no-unused-vars
-      config.module.rules.push({ // eslint-disable-line no-param-reassign
-          test: /\.pdf$/, // eslint-disable-line no-useless-escape
-          use: [ // eslint-disable-line no-param-reassign
-              {
-                  loader: 'file-loader',  // eslint-disable-line no-undef
-                  options: { // eslint-disable-line no-undef
-                      name: '[path][name].[ext]',   // eslint-disable-line no-undef
-                      publicPath: `/_next/static/files`, // eslint-disable-line no-undef
-                      outputPath: `${isServer ? '../' : ''}static/files`,   // eslint-disable-line no-undef
-                  },
-              },
-          ],
-      });
+module.exports = {
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+            publicPath: '/_next/static/files',
+            outputPath: `${isServer ? '../' : ''}static/files`,
+          },
+        },
+      ],
+    });
 
-      return config;
+    return config;
   },
 };
