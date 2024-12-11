136732bea92e12558488da2df4ec1f639aafae27
anassajja <anass.ajja2020@gmail.com>
2024-12-11 11:58:41 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
 // File: next.config.js
 
-module.exports = {
-    assetPrefix: '/_next/static',
-    webpack: (config, { isServer }) => {
-      config.module.rules.push({
-        test: /\.pdf$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-            publicPath: '/_next/static/files',
-            outputPath: `${isServer ? '../' : ''}static/files`,
-          },
-        }],
+module.exports = { // eslint-disable-line no-undef
+  webpack: (config, { isServer }) => { // eslint-disable-line no-unused-vars
+      config.module.rules.push({ // eslint-disable-line no-param-reassign
+          test: /\.pdf$/, // eslint-disable-line no-useless-escape
+          use: [ // eslint-disable-line no-param-reassign
+              {
+                  loader: 'file-loader',  // eslint-disable-line no-undef
+                  options: { // eslint-disable-line no-undef
+                      name: '[path][name].[ext]',   // eslint-disable-line no-undef
+                      publicPath: `/_next/static/files`, // eslint-disable-line no-undef
+                      outputPath: `${isServer ? '../' : ''}static/files`,   // eslint-disable-line no-undef
+                  },
+              },
+          ],
       });
+
       return config;
-    },
-};  
+  },
+};
